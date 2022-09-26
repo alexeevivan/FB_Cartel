3ba656b5bd405a2ccf4f90e7869dfa8c86a04fed
Ivan Alexeev <alexeevivan92@gmail.com>
2022-09-26 05:48:58 +0300

    index.html fix by BEM

--- mysite/static/js/modal.js
+++ mysite/static/js/modal.js
@@ -1,27 +1,27 @@
 var modals = document.getElementsByClassName("modal");
-var modalOpenBtn = document.getElementsByClassName("modalOpenBtn");
+var modal__openbtn = document.getElementsByClassName("modal__open-btn");
 var currentModal = null;
 
 // Function to open modal by id
 function openModal(id) {
-    for (i = 0; i < modals.length; i++) {
-        if (modals[i].getAttribute('id') == id) {
-            currentModal = modals[i];
-            currentModal.style.display = "flex" || currentModal.style.position === "fixed";
-            break;
-        }
-    }
+	for (i = 0; i < modals.length; i++) {
+		if (modals[i].getAttribute('id') == id) {
+			currentModal = modals[i];
+			currentModal.style.display = "flex" || currentModal.style.position === "fixed";
+			break;
+		}
+	}
 }
 
 // When the user clicks the button, open modal with the same id
-modalOpenBtn.onclick = function() {
-    let currentID = modalOpenBtn.getAttribute('id');
-    openModal(currentID);
+modal__openbtn.onclick = function () {
+	let currentID = modal__openbtn.getAttribute('id');
+	openModal(currentID);
 }
 
 // When the user clicks anywhere outside of the modal or the X, close
-window.onclick = function(event) {
-    if (event.target == currentModal || event.target.getAttribute('class') == 'modalClose') {
-        currentModal.style.display = "none";
-    }
+window.onclick = function (event) {
+	if (event.target == currentModal || event.target.getAttribute('class') == 'modal__close') {
+		currentModal.style.display = "none";
+	}
 }
