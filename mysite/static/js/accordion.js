8d2129ddeba609bdd3a0fa9cca785077ae0e4077
Ivan Alexeev <alexeevivan92@gmail.com>
2022-09-28 21:05:05 +0300

    Still adding fixe by BEM and other rules

--- mysite/static/js/accordion.js
+++ mysite/static/js/accordion.js
@@ -1,18 +1,18 @@
-$(function() {
-        $('.acc_ctrl').on('click', function(e) {
-            e.preventDefault();
-            if ($(this).hasClass('active')) {
-                    $(this).removeClass('active');
-                    $(this).next()
-                    .stop()
-                    .slideUp(300);
-                } else {
-                    $(this).addClass('active');
-                    $(this).next()
-                    .stop()
-                    .slideDown(300);
-                }
-            }
-        );
-    }
+$(function () {
+	$('.acc__ctrl').on('click', function (e) {
+		e.preventDefault();
+		if ($(this).hasClass('active')) {
+			$(this).removeClass('active');
+			$(this).next()
+				.stop()
+				.slideUp(300);
+		} else {
+			$(this).addClass('active');
+			$(this).next()
+				.stop()
+				.slideDown(300);
+		}
+	}
+	);
+}
 );
